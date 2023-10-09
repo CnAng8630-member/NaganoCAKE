68c0fea6b32decbe454ccd6c6b609d073e42fad6
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-10-09 10:57:51 +0000

    Add header

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: 'yama110326@gmail.com',
+  password: 'yama6230'
+)
