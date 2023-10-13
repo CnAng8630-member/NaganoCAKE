fafa8bec41e72923ea7704fcd1d1f618b0fb4321
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-10-13 10:38:13 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'homes#top'
+
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+
+    resources :customers, only: [:index, :show, :edit, :update]
+
+    resources :orders, only: [:show]
   end
 
   devise_for :customers, skip: [:passwords], controllers: {
