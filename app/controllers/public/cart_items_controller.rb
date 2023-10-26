253d7186987259c5e91ea141e26db5ebbd93766f
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-10-26 20:15:02 +0000

    Add cart create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,4 +10,10 @@ class Public::CartItemsController < ApplicationController
 
   def index
   end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
 end
