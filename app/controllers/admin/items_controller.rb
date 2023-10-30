d97fc68484dc0c536fd3a3c33776e8f6d2a49d48
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-10-30 14:00:56 +0000

    Add cart_items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,9 +4,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to admin_item_path(@item.id)
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_item_path(item.id)
     else
       render :new
     end
@@ -25,9 +25,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to admin_item_path(@item.id)
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to admin_item_path(item.id)
     else
       render :edit
     end
