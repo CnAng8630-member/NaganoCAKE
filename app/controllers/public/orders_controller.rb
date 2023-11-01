a1f264bbcdd7da4550ac8f1a10c208ca16da39b4
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-11-01 10:41:26 +0000

    Add order_confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,10 +2,24 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def new
+    @order = Order.new
     @customer = current_customer
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @order.shipping_post_code = current_customer.postal_code
+    @order.shipping_address = current_customer.address
+    @order.shipping_name = current_customer.last_name + current_customer.first_name
+    @orders = Order.new
+    @cart_items = CartItem.all
+    @customer = current_customer
+    @total = 0
+    @postage = 800
+    @claim = 0
+  end
+
+  def create
   end
 
   def complete
@@ -15,5 +29,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :shipping_post_code, :shipping_address, :shipping_name)
   end
 end
